8fb19a5a684a0dd73e577c1c4763499cf3550e3e
Tyler Sargent <tsargent@gmail.com>
2015-04-24 10:48:13 -0400

    grid

--- src/js/pages/colors.js
+++ src/js/pages/colors.js
@@ -6,14 +6,15 @@ let D = React.DOM;
 export default React.createClass({
   displayName: "Colors",
   render() {
-    return D.ul(null, Object.keys(ColorData).map((base_color, index) =>
-      D.li(null, { base_color },
-        D.ul(null, Object.keys(ColorData[base_color]).map((variant,index) =>
-          D.li({
-            style: {"background-color": ColorData[base_color][variant]}
+    return D.div(null, Object.keys(ColorData).map((base_color, index) =>
+      D.div(null, { base_color },
+        D.div(null, Object.keys(ColorData[base_color]).map((variant,index) =>
+          D.div({
+            className: 'row'
           }, [
-            `.${base_color}-${variant} ${ColorData[base_color][variant]}`,
-              D.code({className: 'language-css'},`.${base_color}-${variant}`)
+            D.div({className: 'col-1', style: {"background-color": ColorData[base_color][variant]}}, "\u00a0"),
+            D.div({className: 'col-2'}, `${ColorData[base_color][variant]}`),
+            D.div({className: 'col-2'}, D.code({className: 'language-css'},`.${base_color}-${variant}`))
           ])
         ))
     )))
