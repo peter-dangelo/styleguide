7279fad1648f11a4fe81c4d35c8495bf049c4fad
Tyler Sargent <tsargent@gmail.com>
2015-04-23 20:47:14 -0400

    a colors component to display data from the json

---
+++ src/js/pages/colors.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ColorData from '../../lib/colors'
+
+let D = React.DOM;
+
+export default React.createClass({
+  displayName: "Colors",
+  render() {
+    return D.ul(null, Object.keys(ColorData).map((base_color, index) =>
+      D.li(null, { base_color },
+        D.ul(null, Object.keys(ColorData[base_color]).map((variant,index) =>
+          D.li(null, variant, D.span(null, {}, ColorData[base_color][variant]))
+        ))
+    )))
+  }
+});
+
+
