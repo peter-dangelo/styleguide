1cbba8856ba2d6070c91b512a920a1b032b2f2a9
Nick Hehr <headhipster@hipsterbrown.com>
2015-04-29 14:50:26 -0400

    Updates color.js to display all color utility classes.

--- src/js/pages/colors.js
+++ src/js/pages/colors.js
@@ -5,18 +5,23 @@ let D = React.DOM;
 
 export default React.createClass({
   displayName: "Colors",
+
   render() {
-    return D.div(null, Object.keys(ColorData).map((base_color, index) =>
-      D.div(null, { base_color },
-        D.div(null, Object.keys(ColorData[base_color]).map((variant,index) =>
+    return D.section(null, Object.keys(ColorData).map((base_color, index) =>
+      D.article({className: "mb3"}, null,
+        D.h3({}, base_color),
+        Object.keys(ColorData[base_color]).map((variant,index) =>
           D.div({
-            className: 'row'
+            className: 'row mb2'
           }, [
-            D.div({className: 'col-1', style: {"background-color": ColorData[base_color][variant]}}, "\u00a0"),
-            D.div({className: 'col-2'}, `${ColorData[base_color][variant]}`),
-            D.div({className: 'col-2'}, D.code({className: 'language-css'},`.${base_color}-${variant}`))
+            D.div({className: `col-1 bg-${base_color}-${variant}`}, "\u00a0"),
+            D.div({className: `col-1 ${base_color}-${variant}`}, `${ColorData[base_color][variant]}`),
+            D.div({className: `col-1 b bw-1 bc-${base_color}-${variant}`}, "\u00a0"),
+            D.div({className: 'col-2'}, D.code({className: 'language-css'},`.bg-${base_color}-${variant}`)),
+            D.div({className: 'col-2'}, D.code({className: 'language-css'},`.${base_color}-${variant}`)),
+            D.div({className: 'col-2'}, D.code({className: 'language-css'},`.bc-${base_color}-${variant}`))
           ])
-        ))
+        )
     )))
   }
 });
