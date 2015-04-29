7e3538e991425f441c58d25ca8cde109cac8e2d9
Keith Poplawski <keith.poplawski@gmail.com>
2015-04-29 18:41:52 -0400

    displays example button components in styleguide

--- src/js/pages/buttons.js
+++ src/js/pages/buttons.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import Styleguide from '../styleguide';
-
-let D = React.DOM;
+import Button from '../components/button';
 
 export default React.createClass({
   displayName: "ButtonsPage",
 
   render() {
-    return React.createElement(Styleguide, {
-      title: "Buttons Styles"
-    }, React.createElement("div", {
-      title: "Paragraph",
-      description: "The paragraph styles for Namely app",
-      example: 'D.p({}, "Some sample text for this paragraph component")'
-    }, React.createElement("p", {}, "Some sample text for this paragraph component"))
-    );
+    return <Styleguide title="Button Styles">
+
+      <div title="Base" description="The button styles for Namely app">
+        <Button label="Base" />
+      </div>
+
+      <div title="Types">
+        <Button label=".button" />
+        <Button label=".button-secondary" type="secondary" />
+        <Button label=".button-danger" type="danger" />
+        <Button label=".button-disabled" type="disabled" />
+      </div>
+
+      <div title="Sizes">
+        <Button label="Default Size" />
+        <Button label=".button-sm" size="sm" />
+        <Button label=".button-sm.button-danger" size="sm" type="danger" />
+      </div>
+    </Styleguide>
   }
 });
-
-
