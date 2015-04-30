a26b18ab96d789ef0c97c9a323c1d7423b0862b6
Keith Poplawski <keith.poplawski@gmail.com>
2015-04-30 18:21:01 -0400

    some sloppy spacing between example buttons

--- src/js/pages/buttons.js
+++ src/js/pages/buttons.js
@@ -14,31 +14,59 @@ export default React.createClass({
 
       <div title="Types">
         <Button label=".button" />
+        <br />
+        <br />
         <Button label=".button-secondary" type="secondary" />
+        <br />
+        <br />
         <Button label=".button-danger" type="danger" />
+        <br />
+        <br />
         <Button label=".button:disabled" disabled={ true } />
       </div>
 
       <div title="Sizes">
         <Button label="Default Size" />
+        <br />
+        <br />
         <Button label=".button-sm" size="sm" />
+        <br />
+        <br />
         <Button label=".button-sm.button-danger" size="sm" type="danger" />
+        <br />
+        <br />
         <Button label=".button-sm:disabled" size="sm" disabled={ true } />
       </div>
 
       <div title="Links">
         <Button label=".button-link" link={ true } />
+        <br />
+        <br />
         <Button label=".button-link.button-secondary" type="secondary" link={ true } />
+        <br />
+        <br />
         <Button label=".button-link.button-danger" type="danger" link={ true } />
+        <br />
+        <br />
         <Button label=".button-link:disabled" link={ true } disabled={ true } />
+        <br />
+        <br />
         <Button label=".button-link.button-sm" size="sm" link={ true } />
+        <br />
+        <br />
         <Button label=".button-link.button-sm.button-secondary" size="sm" type="secondary" link={ true } />
       </div>
 
       <div title="Directions">
         <Button label=".button-previous" type="previous" />
+        <br />
+        <br />
         <Button label=".button-next" type="next" />
+        <br />
+        <br />
         <Button label=".button-previous:disabled" type="previous" disabled={ true } />
+        <br />
+        <br />
         <Button label=".button-next:disabled" type="next" disabled={ true } />
       </div>
 
