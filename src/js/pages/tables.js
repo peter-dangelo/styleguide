56e33099acaee6d09c701373026d1de3af98235f
Tyler Sargent <tsargent@gmail.com>
2015-04-30 10:08:44 -0400

    a simple table

--- src/js/pages/tables.js
+++ src/js/pages/tables.js
@@ -7,15 +7,34 @@ export default React.createClass({
   displayName: "TablesPage",
 
   render() {
-    return React.createElement(Styleguide, {
-      title: "Tables + List Styles"
-    }, React.createElement("div", {
-      title: "Paragraph",
-      description: "The paragraph styles for Namely app",
-      example: 'D.p({}, "Some sample text for this paragraph component")'
-    }, React.createElement("p", {}, "Some sample text for this paragraph component"))
-    );
+    return <Styleguide>
+      <div title="Paragraph" description="The paragraph styles for Namely">
+        <table>
+          <thead>
+            <tr>
+              <th>Col 1</th>
+              <th>Col 2</th>
+              <th>Col 3</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>some</td>
+              <td>data</td>
+              <td>here</td>
+            </tr>
+            <tr>
+              <td>more</td>
+              <td>stuff</td>
+              <td>here</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </Styleguide>
   }
+
+
 });
 
 
