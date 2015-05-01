6d7aa96cd1d00ea9f45e57248bbcc3ad4a14ed3d
Tyler Sargent <tsargent@gmail.com>
2015-05-01 11:51:23 -0400

    inputs

--- src/js/pages/forms.js
+++ src/js/pages/forms.js
@@ -6,15 +6,39 @@ let D = React.DOM;
 export default React.createClass({
   displayName: "FormsPage",
 
+  // render() {
+  //   return React.createElement(Styleguide, {
+  //     title: "Forms Styles"
+  //   }, React.createElement("div", {
+  //     title: "Form",
+  //     description: "The form and input styles for Namely",
+  //     example: 'D.p({}, "Some sample text for this paragraph component")'
+  //   }, React.createElement("p", {}, "Some sample text for this paragraph component"))
+  //   );
+  // }
+
   render() {
-    return React.createElement(Styleguide, {
-      title: "Forms Styles"
-    }, React.createElement("div", {
-      title: "Paragraph",
-      description: "The paragraph styles for Namely app",
-      example: 'D.p({}, "Some sample text for this paragraph component")'
-    }, React.createElement("p", {}, "Some sample text for this paragraph component"))
-    );
+    return <Styleguide title="Forms Styles">
+        <div title="Form" description="The form and input styles for Namely">
+          <form>
+            <h3>This is a form</h3>
+
+            <h5 className="mb0">Checkboxes</h5>
+            <input type="checkbox"></input>
+
+            <h5 className="mb0">Select</h5>
+            <select>
+              <option>Option 1</option>
+              <option>Option 2</option>
+            </select>
+
+
+
+
+          </form>
+
+        </div>
+      </Styleguide>
   }
 });
 
