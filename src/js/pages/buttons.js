fe96d5fa687da207610964581bb1e6a590fac8ee
Keith Poplawski <keith.poplawski@gmail.com>
2015-05-01 12:34:48 -0400

    refactors and adds utility classes to example button components

--- src/js/pages/buttons.js
+++ src/js/pages/buttons.js
@@ -13,60 +13,32 @@ export default React.createClass({
       </div>
 
       <div title="Types">
-        <Button label=".button" />
-        <br />
-        <br />
-        <Button label=".button-secondary" type="secondary" />
-        <br />
-        <br />
-        <Button label=".button-danger" type="danger" />
-        <br />
-        <br />
+        <Button label=".button" extraClass="mr3" />
+        <Button label=".button-secondary" type="secondary" extraClass="mr3" />
+        <Button label=".button-danger" type="danger" extraClass="mr3" />
         <Button label=".button:disabled" disabled={ true } />
       </div>
 
       <div title="Sizes">
-        <Button label="Default Size" />
-        <br />
-        <br />
-        <Button label=".button-sm" size="sm" />
-        <br />
-        <br />
-        <Button label=".button-sm.button-danger" size="sm" type="danger" />
-        <br />
-        <br />
+        <Button label="Default Size" extraClass="mr3" />
+        <Button label=".button-sm" size="sm" extraClass="mr3" />
+        <Button label=".button-sm.button-danger" size="sm" type="danger" extraClass="mr3" />
         <Button label=".button-sm:disabled" size="sm" disabled={ true } />
       </div>
 
       <div title="Links">
-        <Button label=".button-link" link={ true } />
-        <br />
-        <br />
-        <Button label=".button-link.button-secondary" type="secondary" link={ true } />
-        <br />
-        <br />
-        <Button label=".button-link.button-danger" type="danger" link={ true } />
-        <br />
-        <br />
-        <Button label=".button-link:disabled" link={ true } disabled={ true } />
-        <br />
-        <br />
-        <Button label=".button-link.button-sm" size="sm" link={ true } />
-        <br />
-        <br />
+        <Button label=".button-link" link={ true } extraClass="mr3" />
+        <Button label=".button-link.button-secondary" type="secondary" link={ true } extraClass="mr3" />
+        <Button label=".button-link.button-danger" type="danger" link={ true } extraClass="mr3" />
+        <Button label=".button-link:disabled" link={ true } disabled={ true } extraClass="mr3" />
+        <Button label=".button-link.button-sm" size="sm" link={ true } extraClass="mr3" />
         <Button label=".button-link.button-sm.button-secondary" size="sm" type="secondary" link={ true } />
       </div>
 
       <div title="Directions">
-        <Button label=".button-previous" type="previous" />
-        <br />
-        <br />
-        <Button label=".button-next" type="next" />
-        <br />
-        <br />
-        <Button label=".button-previous:disabled" type="previous" disabled={ true } />
-        <br />
-        <br />
+        <Button label=".button-previous" type="previous" extraClass="mr3" />
+        <Button label=".button-next" type="next" extraClass="mr3" />
+        <Button label=".button-previous:disabled" type="previous" disabled={ true } extraClass="mr3" />
         <Button label=".button-next:disabled" type="next" disabled={ true } />
       </div>
 
