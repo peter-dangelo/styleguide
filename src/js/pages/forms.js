feda0c1be0d62449bf96f250aed665e8717fc576
Tyler Sargent <tsargent@gmail.com>
2015-05-01 17:24:14 -0400

    form page

--- src/js/pages/forms.js
+++ src/js/pages/forms.js
@@ -14,19 +14,22 @@ export default React.createClass({
             <hr />
 
             <h3>Text inputs</h3>
+            <p>Set their widths with col classes.</p>
 
             <div className="flex flex-justify">
               <div>
-                <label htmlFor="company_name" className="px2 mb1">Company Name</label>
-                <input id="company_name" type="text" placeholder="Company Name"></input>
+                <label htmlFor="company_name" className="px2 mb1">Text field</label>
+                <input id="company_name" type="text" placeholder="Text field"></input>
               </div>
               <div>
-                <label htmlFor="number_field" className="px2 mb1">Number Field</label>
-                <input id="number_field" type="number"></input>
+                <label htmlFor="number_field" className="px2 mb1">Number</label>
+                <input className="col-3" id="number_field" type="number"></input>
+                <p className="orange">Arrow icons needed</p>
               </div>
               <div>
                 <label htmlFor="date_field" className="px2 mb1">Date Field</label>
                 <input id="date_field" type="text"></input>
+                <p className="orange">Calendar icon needed</p>
               </div>
 
 
@@ -41,25 +44,34 @@ export default React.createClass({
             <hr />
 
             <h3>Checkboxes</h3>
+            <p className="orange">Check icon needed</p>
             <div className="flex flex-justify">
               <div>
                 <p>Default</p>
-                <input type="checkbox"></input>
+                <div className="flex flex-wrap">
+                  <div><input type="checkbox" id="checkbox_1"></input></div>
+                  <div><label className="ml2" htmlFor="checkbox_1">Label</label></div>
+                </div>
               </div>
               <div>
                 <p>Read-only, checked</p>
-                <input type="checkbox" readOnly="true" checked="true"></input>
+                <div className="flex flex-wrap">
+                  <div><input type="checkbox" id="checkbox_2" readOnly="true" checked="true"></input></div>
+                  <div><label className="right ml2" htmlFor="checkbox_2">Label</label></div>
+                </div>
               </div>
             </div>
 
             <hr />
 
             <h3>Radio buttons</h3>
+
             <input type="radio" name="radio"></input>
 
             <hr />
 
             <h3>Select</h3>
+            <p className="orange">Arrow icons needed</p>
             <select>
               <option>Option 1</option>
               <option>Option 2</option>
