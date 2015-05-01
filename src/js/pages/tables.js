1c09bd67124820465284db1c159ab9f349e4f7f0
Tyler Sargent <tsargent@gmail.com>
2015-05-01 10:49:14 -0400

    correct type

--- src/js/pages/tables.js
+++ src/js/pages/tables.js
@@ -56,20 +56,20 @@ export default React.createClass({
         <table>
           <thead>
             <tr>
-              <th className="center"><input type="checkBox"></input></th>
+              <th className="center"><input type="checkbox"></input></th>
               <th className="center">Checkbox</th>
               <th>Header</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td className="center"><input type="checkBox"></input></td>
-              <td className="center"><input type="checkBox"></input></td>
+              <td className="center"><input type="checkbox"></input></td>
+              <td className="center"><input type="checkbox"></input></td>
               <td>Suspendisse quis dictum nulla, quis hendrerit felis</td>
             </tr>
             <tr>
-              <td className="center"><input type="checkBox"></input></td>
-              <td className="center"><input type="checkBox" readOnly="true" checked="true"></input></td>
+              <td className="center"><input type="checkbox"></input></td>
+              <td className="center"><input type="checkbox" readOnly="true" checked="true"></input></td>
               <td>The second checkbox is checked and read-only.</td>
             </tr>
           </tbody>
