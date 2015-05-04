baee83e6060f5d4d8ab829aea504ec6774d41a3b
Keith Poplawski <keith.poplawski@gmail.com>
2015-05-04 14:07:21 -0400

    adds button group component

---
+++ src/js/components/button-group.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const Type = React.PropTypes;
+
+export default React.createClass({
+  displayName: "ButtonGroup",
+
+  propTypes: {
+   type: Type.oneOf(['actions']).isRequired,
+   children: Type.node.isRequired,
+   extraClass: Type.string
+  },
+
+  getDefaultProps: function() {
+    return {
+      type: "actions"
+    };
+  },
+
+  createClass: function(value) {
+    return "button-group-" + value;
+  },
+
+  classes: function() {
+    var classes = ["flex"];
+
+    if (this.props.type) {
+      classes.push(this.createClass(this.props.type));
+    }
+
+    if (this.props.extraClass) {
+      classes.push(this.props.extraClass);
+    }
+
+    return classes.join(' ');
+  },
+
+  render() {
+    return <div className={ this.classes() } >{ this.props.children }</div>;
+  }
+});
