233beaef772c7b9032951e07f343b34d2bdcd102
Tyler Sargent <tsargent@gmail.com>
2015-05-04 14:27:19 -0400

    form additions

--- src/js/pages/forms.js
+++ src/js/pages/forms.js
@@ -23,12 +23,9 @@ export default React.createClass({
     return <Styleguide title="Forms Styles">
         <div title="Form" description="The form and input styles for Namely">
           <form>
-
             <hr />
-
             <h3>Text inputs</h3>
             <p>Set their widths with col classes.</p>
-
             <div className="flex flex-justify">
               <div>
                 <label htmlFor="company_name" className="px2 mb1">Text field</label>
@@ -44,18 +41,12 @@ export default React.createClass({
                 <input id="date_field" type="text"></input>
                 <p className="orange">Calendar icon needed</p>
               </div>
-
-
             </div>
-
             <hr />
-
             <h3>Textarea</h3>
             <label htmlFor="bio" className="px2 mb1">Bio</label>
             <textarea id="bio"></textarea>
-
             <hr />
-
             <h3>Checkboxes</h3>
             <p className="orange">Check icon needed</p>
             <div className="flex flex-justify">
@@ -78,12 +69,9 @@ export default React.createClass({
                 </div>
               </div>
             </div>
-
             <hr />
-
             <h3>Radio buttons</h3>
             <p className="orange">Checked (bullet?) icon needed</p>
-
             <div className="flex flex-wrap mb1">
               <div><input type="radio" id="radio_1" name="radios"></input></div>
               <div><label className="ml2" htmlFor="radio_1">Label 1</label></div>
@@ -92,21 +80,55 @@ export default React.createClass({
               <div><input type="radio" id="radio_2" name="radios"></input></div>
               <div><label className="ml2" htmlFor="radio_2">Label 2</label></div>
             </div>
-
             <hr />
-
             <h3>Select</h3>
             <p className="orange">Arrow icons needed</p>
-            <div className="col-3">
-              <label htmlFor="select_1" className="px2 mb1">Select</label>
-              <select id="select_1" className="bc-grey-25">
-                <option>Option 1</option>
-                <option>Option 2</option>
-              </select>
+            <div className="py3">
+              <h4>Native</h4>
+              <div className="col-3">
+                <label htmlFor="select_1" className="px2 mb1">Select</label>
+                <select id="select_1" className="bc-grey-25">
+                  {options.map(function(option){
+                    return <option value={option.value}>{option.label}</option>;
+                  })}
+                </select>
+              </div>
             </div>
-
+            <div className="py3">
+              <h4>React Select</h4>
+              <p><a href="https://github.com/JedWatson/react-select">https://github.com/JedWatson/react-select</a></p>
+              <div className="col-3 left">
+                <label className="px2 mb1">Default</label>
+                <Select
+                  searchable={false}
+                  name="form-field-nameczXCzx"
+                  options={options}
+                  onChange={logChange}
+                  placeholder="- Select Office -"
+                />
+              </div>
+              <div className="col-3 left">
+                <label className="px2 mb1">Multi</label>
+                <Select
+                  searchable={false}
+                  multi={true}
+                  name="form-field-nameczXCzx"
+                  options={options}
+                  onChange={logChange}
+                  placeholder="- Select Office -"
+                />
+              </div>
+              <div className="col-3 left">
+                <label className="px2 mb1">Searchable (like Chosen)</label>
+                <Select
+                  name="form-field-nameczXCzx"
+                  options={options}
+                  onChange={logChange}
+                  placeholder="- Select Office -"
+                />
+              </div>
+            </div>
           </form>
-
         </div>
       </Styleguide>
   }
