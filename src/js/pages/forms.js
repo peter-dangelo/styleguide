c003c97559539d2b6d1db07600ea958e7fdb5367
Tyler Sargent <tsargent@gmail.com>
2015-05-05 16:28:07 -0400

    moves some markup out to components

--- src/js/pages/forms.js
+++ src/js/pages/forms.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Styleguide from '../styleguide';
 import Select from 'react-select';
+import DateField from '../components/forms/fields/date';
+import NumberField from '../components/forms/fields/number';
+import TextField from '../components/forms/fields/text';
+import TextArea from '../components/forms/fields/text_area';
+import Checkbox from '../components/forms/fields/checkbox';
+import Radio from '../components/forms/fields/radio';
+import SimpleSelect from '../components/forms/fields/simple_select';
 
 let D = React.DOM;
-
 
 var options = [
     { value: 'london', label: 'London' },
@@ -31,69 +37,38 @@ export default React.createClass({
             <h3>Text inputs</h3>
             <p>Set their widths with col classes.</p>
             <div className="flex flex-justify">
-              <div>
-                <label htmlFor="company_name" className="px2 mb1">Text field</label>
-                <input id="company_name" type="text" placeholder="Text field"></input>
-              </div>
-              <div>
-                <label htmlFor="number_field" className="px2 mb1">Number</label>
-                <input className="col-3" id="number_field" type="number"></input>
-                <p className="orange">Arrow icons needed</p>
-              </div>
-              <div>
-                <label htmlFor="date_field" className="px2 mb1">Date Field</label>
-                <input id="date_field" type="text"></input>
-                <p className="orange">Calendar icon needed</p>
-              </div>
+              <TextField label="Text" placeholder="Placeholder" />
+              <NumberField label="Number"/>
+              <DateField label="Date"/>
             </div>
             <hr />
             <h3>Textarea</h3>
-            <label htmlFor="bio" className="px2 mb1">Bio</label>
-            <textarea id="bio"></textarea>
+            <TextArea label="Bio" />
             <hr />
             <h3>Checkboxes</h3>
             <div className="flex flex-justify">
               <div>
                 <p>Default</p>
-                <div className="flex flex-wrap mb1">
-                  <div><input type="checkbox" id="checkbox_1"></input></div>
-                  <div><label className="ml2" htmlFor="checkbox_1">Label 1</label></div>
-                </div>
-                <div className="flex flex-wrap">
-                  <div><input type="checkbox" id="checkbox_2"></input></div>
-                  <div><label className="ml2" htmlFor="checkbox_2">Label 2</label></div>
-                </div>
+                <Checkbox label="Checkbox 1" />
+                <Checkbox label="Checkbox 2" />
               </div>
               <div>
                 <p>Read-only, checked</p>
-                <div className="flex flex-wrap">
-                  <div><input type="checkbox" id="checkbox_3" readOnly="true" checked="true"></input></div>
-                  <div><label className="right ml2" htmlFor="checkbox_3">Label</label></div>
-                </div>
+                <Checkbox label="Checkbox 3" readOnly={true} checked={true} />
               </div>
             </div>
             <hr />
             <h3>Radio buttons</h3>
-            <div className="flex flex-wrap mb1">
-              <div><input type="radio" id="radio_1" name="radios"></input></div>
-              <div><label className="ml2" htmlFor="radio_1">Label 1</label></div>
-            </div>
-            <div className="flex flex-wrap">
-              <div><input type="radio" id="radio_2" name="radios"></input></div>
-              <div><label className="ml2" htmlFor="radio_2">Label 2</label></div>
-            </div>
+
+            <Radio name="radios" label="Radio 1"/>
+            <Radio name="radios" label="Radio 2"/>
+
             <hr />
+
             <h3>Select</h3>
             <div className="py3">
               <h4>Native</h4>
-              <div className="col-3">
-                <label htmlFor="select_1" className="px2 mb1">Select</label>
-                <select id="select_1" className="bc-grey-25">
-                  {options.map(function(option){
-                    return <option value={option.value}>{option.label}</option>;
-                  })}
-                </select>
-              </div>
+              <SimpleSelect label="Simple Select" options={options} />
             </div>
             <div className="py3">
               <h4>React Select</h4>
