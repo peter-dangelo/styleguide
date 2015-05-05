c1713f28c8784ea6181166ce8d0c28a6e425e77b
Keith Poplawski <keith.poplawski@gmail.com>
2015-05-05 18:17:38 -0400

    cleans up iconpage view for styleguide

--- src/js/pages/icons.js
+++ src/js/pages/icons.js
@@ -6,17 +6,14 @@ import IconData from '../../lib/_icons';
 export default React.createClass({
   displayName: "IconsPage",
 
-  generateIcons: function(data) {
-    data.map(function(object) {
-      return <Icon name={object.name} />
-    });
-  },
-
   render() {
     return <Styleguide title="Icons">
       <div title="Names">
         {IconData.map(function(object, i){
-            return <div><Icon name={object.name} key={i} /><code className="code">.icon-{object.name}</code></div>;
+          return <div>
+            <Icon name={object.name} key={i} />
+            <code>.icon-{object.name}</code>
+          </div>;
         })}
       </div>
     </Styleguide>
