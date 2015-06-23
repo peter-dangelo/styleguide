65db24b711ce8dbbdd0e867565440e2322f8eaf8
Tyler Sargent <tsargent@gmail.com>
2015-06-23 15:11:40 -0400

    adds fade class for animation

--- src/js/components/modal/modal-background.js
+++ src/js/components/modal/modal-background.js
@@ -15,7 +15,7 @@ export default React.createClass({
   },
 
   render() {
-    return <div key="modalBackground" style={this.styles} className="modal-background top-0 bottom-0 left-0 right-0 fixed flex flex-center" onClick={this.handleClick}>
+    return <div key="modalBackground" style={this.styles} className="fade modal-background top-0 bottom-0 left-0 right-0 fixed flex flex-center" onClick={this.handleClick}>
         <ModalContent {...this.props}/>
       </div>
 
