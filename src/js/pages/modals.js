94824754bb224091a323859068ebc35f3cc9705e
Tyler Sargent <tsargent@gmail.com>
2015-06-25 13:23:02 -0400

    uses the props-table component

--- src/js/pages/modals.js
+++ src/js/pages/modals.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Styleguide from '../styleguide';
+import PropsTable from '../style_guide_components/props-table';
 import Modal from '../components/modal/modal';
 
 let D = React.DOM;
@@ -11,26 +12,39 @@ export default React.createClass({
     return <Styleguide title="Modals">
       <div title="Modals" description="The modal styles for Namely">
 
-        <p>Wrap a component inside the modal component. Props:<br/>
-        <code className="language-javascript">prompt="Click me"</code>for the button text<br/>
-        <code className="language-javascript">contentColumnWidth="3"</code> to override the default 9-col width<br/>
-        <code className="language-javascript">isOpen=true</code> to automatically open the modal when the modal button renders<br/>
-        </p>
+        <p>Wrap a component inside the modal component.</p>
+
+        <PropsTable rows={
+          [
+            {
+              'prop':'prompt',
+              'description':'sets the button prompt'
+            },
+            {
+              'prop':'contentColumnWidth',
+              'description':'sets the width of the content container with columns. Default: 9'
+            },
+            {
+              'prop':'isOpen',
+              'description':'automatically opens the modal when the modal button renders. Sets modal-background\'s inital state to isOpen:true. Default: false'
+            }
+          ]
+        }/>
 
         <hr />
 
         <Modal prompt="Launch default modal">
-          <h3 className="mb2">I&apos;m a modal!</h3>
+          <h3 className="mb2">{"I'm a modal!"}</h3>
           <hr className="mt0" />
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id ante ut erat vulputate ultricies ac in turpis. Curabitur mollis quis erat ut sodales. Sed ut pellentesque mauris. Donec lectus augue, sodales ut diam et, consequat condimentum metus. Sed faucibus congue mi. Pellentesque eu pharetra est.</p>
+          <p>{"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id ante ut erat vulputate ultricies ac in turpis. Curabitur mollis quis erat ut sodales. Sed ut pellentesque mauris. Donec lectus augue, sodales ut diam et, consequat condimentum metus. Sed faucibus congue mi. Pellentesque eu pharetra est."}</p>
         </Modal>
 
         <hr />
 
         <Modal prompt="Launch 3-col modal" contentColumnWidth="3">
-          <div>I&apos;m a 3-column modal!</div>
+          <div>{"I'm a 3-column modal!"}</div>
           <hr />
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id ante ut erat vulputate ultricies ac in turpis. Curabitur mollis quis erat ut sodales. Sed ut pellentesque mauris. Donec lectus augue, sodales ut diam et, consequat condimentum metus. Sed faucibus congue mi. Pellentesque eu pharetra est.</p>
+          <p>{"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id ante ut erat vulputate ultricies ac in turpis. Curabitur mollis quis erat ut sodales. Sed ut pellentesque mauris. Donec lectus augue, sodales ut diam et, consequat condimentum metus. Sed faucibus congue mi. Pellentesque eu pharetra est."}</p>
         </Modal>
 
       </div>
