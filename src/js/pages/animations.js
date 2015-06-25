7c90d5f38ffa6ec93fd08be0ce9c0a96741466ed
Tyler Sargent <tsargent@gmail.com>
2015-06-25 12:08:33 -0400

    use anim- prefix

--- src/js/pages/animations.js
+++ src/js/pages/animations.js
@@ -10,9 +10,9 @@ export default React.createClass({
         <div title="Animations" description="Animations">
           <div calssName="container">
             <div className="col-2">
-              <code className="language-css">.fade</code>
+              <code className="language-css">.admim-fade</code>
             </div>
-            <div className="col-2"><p className="fade">Fade</p></div>
+            <div className="col-2"><p className="anim-fade">Fade</p></div>
           </div>
         </div>
       </Styleguide>
