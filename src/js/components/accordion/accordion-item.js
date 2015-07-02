bfc764607548ef221bd65691e8038404ab0ef2df
Tyler Sargent <tsargent@gmail.com>
2015-07-01 20:55:44 -0400

    imported from namely app pto feature

---
+++ src/js/components/accordion/accordion-item.js
@@ -0,0 +1,93 @@
+import React from 'react';
+
+const Type = React.PropTypes;
+
+export default React.createClass({
+
+  displayName: "AccordionItem",
+
+  propTypes: {
+    isOpen: Type.bool
+  },
+
+  getDefaultProps() {
+    return {
+      isOpen: false
+    };
+  },
+
+  getInitialState() {
+    return {
+      isOpen: this.props.isOpen
+    }
+  },
+
+  // this sets the state when the expand all / collapse all in the parent passes isOpen prop
+  componentWillReceiveProps(nextProps) {
+    this.setState({isOpen: nextProps.isOpen})
+  },
+
+  handleClick(){
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  },
+
+  iconClasses() {
+    var classes = [
+      'mr1'
+    ];
+    this.state.isOpen ? classes.push('icon-collapse','blue') : classes.push('icon-expand','blue');
+    return classes.join(' ');
+  },
+
+  headerClasses() {
+    var classes = [
+      'accordion-item-head',
+      'pointer',
+      'flex',
+      'flex-center',
+      'py2'
+    ];
+
+    return classes.join(' ');
+
+  },
+
+  contentClasses() {
+    var classes = [
+      'accordion-item-content'
+    ]
+    this.state.isOpen ? classes.push('open') : classes.push('close');
+    return classes.join(' ');
+  },
+
+  containerClasses() {
+    var classes = [
+      'bt',
+      'bw-1',
+      'bc-blue-baby',
+      'px2'
+    ];
+
+    if(this.props.isLast) {
+      classes.push('bb');
+    }
+
+    return classes.join(' ');
+
+  },
+
+  render() {
+    return (
+      <div className={this.containerClasses()}>
+        <div className={this.headerClasses()} onClick={this.handleClick}>
+          <span className={this.iconClasses()}/>
+          {this.props.title}
+        </div>
+        <div className={this.contentClasses()}>{this.props.children}</div>
+      </div>
+    );
+  }
+
+});
