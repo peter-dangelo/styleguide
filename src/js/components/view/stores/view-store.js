b26bb4ee8bf67cbe7cb897eb2680af8f60cda491
Keith Poplawski <keith.poplawski@gmail.com>
2015-07-13 18:01:41 -0400

    copies view store from main app

---
+++ src/js/components/view/stores/view-store.js
@@ -0,0 +1,45 @@
+var alt = require('lib/alt');
+
+var ViewActions = require('../actions/view-actions');
+
+class ViewStore {
+  constructor() {
+    this.actionBar = false;
+    this.actionBarTitle = null;
+    this.actionBarDescription = null;
+    this.actionBarActions = [];
+    this.actionBarShow = false;
+
+    // refactor with alt helper
+    this.bindListeners({
+      handleUpdateActionBarTitle: ViewActions.UPDATE_ACTION_BAR_TITLE,
+      handleUpdateActionBarDescription: ViewActions.UPDATE_ACTION_BAR_DESCRIPTION,
+      handleUpdateActionBarActions: ViewActions.UPDATE_ACTION_BAR_ACTIONS,
+      handleShowActionBar: ViewActions.SHOW_ACTION_BAR,
+    });
+  }
+
+
+  handleUpdateActionBarTitle(string) {
+    this.actionBarTitle = string;
+    this.actionBar = true;
+  }
+
+  handleUpdateActionBarDescription(obj) {
+    this.actionBarDescription = obj;
+    this.actionBar = true;
+  }
+
+  handleUpdateActionBarActions(array) {
+    this.actionBarActions = array;
+    this.actionBar = true;
+  }
+
+  handleShowActionBar(bool) {
+    if (this.actionBarShow !== bool) {
+      this.actionBarShow = bool;
+    }
+  }
+}
+
+module.exports = alt.createStore(ViewStore, 'ViewStore');
