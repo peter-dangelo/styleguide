9f66b8b93f7ed521fc3a147f2c0aa0b007f84a37
Keith Poplawski <keith.poplawski@gmail.com>
2015-07-13 15:55:29 -0400

    copies action bar code from main app

---
+++ src/js/components/view/bars/action-bar.js
@@ -0,0 +1,56 @@
+import React from 'react/addons';
+
+const {
+  createClass,
+} = React;
+
+const {
+  cloneWithProps,
+} = React.addons;
+
+const ActionBar = createClass({
+
+  displayName: "ActionBar",
+
+  title() {
+    if (this.props.title) {
+      return <div className="flex-auto"><h3 className="m0">{this.props.title}</h3></div>;
+    }
+  },
+
+  description() {
+    if (this.props.description) {
+      return <div className="flex-auto" style={{textAlign: 'right'}} dangerouslySetInnerHTML={{__html: this.props.description}}></div>;
+
+    }
+  },
+
+  actions() {
+    if (this.props.actions.length > 0) {
+
+      var actions = this.props.actions;
+
+      actions = actions.map( (action) => {
+        return cloneWithProps(action, {className: "button-sm"})
+      });
+
+      return actions;
+    }
+  },
+
+  render() {
+
+    return (
+      <div className="fixed top-0 fill" style={{background: 'rgba(255,255,255,0.95)', zIndex: 99, boxShadow: '0 2px 5px rgba(20,33,45, 0.1)'}}>
+        <div className="flex flex-center p2 mt0 mb0 mx-auto" style={{width: 944}}>
+          {this.title()}
+          {this.description()}
+          <div className="ml2">{this.actions()}</div>
+        </div>
+      </div>
+    );
+  }
+});
+
+
+module.exports = ActionBar;
