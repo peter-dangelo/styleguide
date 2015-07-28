b2da92246d1d96ab0b51fde8c23c0914662f63e4
Keith Poplawski <keith.poplawski@gmail.com>
2015-07-28 11:12:03 -0400

    refactors and updates alt actions to use alt action generator

--- src/js/components/view/actions/view-actions.js
+++ src/js/components/view/actions/view-actions.js
@@ -1,35 +1,11 @@
-var alt = require('../../../alt');
+import alt from '../../../alt';
 
-class ViewActions {
-
-  scrollWatch(bool) {
-    this.dispatch(bool);
-  }
-
-  scrolling() {
-    this.dispatch();
-  }
-
-  updateActionBarTitle(string) {
-    this.dispatch(string);
-  }
-
-  updateActionBarDescription(obj) {
-    this.dispatch(obj);
-  }
-
-  updateActionBarActions(array) {
-    this.dispatch(array);
-  }
-
-  showActionBar(bool) {
-    this.dispatch(bool);
-  }
-
-  scrollListenTo(elem) {
-    this.dispatch(elem)
-  }
-
-}
-
-module.exports = alt.createActions(ViewActions);
+export default alt.generateActions(
+  'scrollWatch',
+  'scrolling',
+  'createActionBar',
+  'setDimensions',
+  'showActionBar',
+  'scrollListenTo',
+  'toggleActionBar'
+);
