3209e33d519bbae0ef34a6185ab4a79596b806d6
Jason Fellows <jasonfellows@gmail.com>
2015-09-18 18:14:11 -0400

    .

--- src/js/components/forms/fields/date/month_picker.js
+++ src/js/components/forms/fields/date/month_picker.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-var daysOfWeek = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa",]
+var daysOfWeek = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
 
 var MonthPicker = React.createClass({
   getDefaultProps: function() {
