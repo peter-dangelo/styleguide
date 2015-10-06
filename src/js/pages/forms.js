09ad8efd3074fbb4aeab683ef7276f1a0f85fe90
Tyler Sargent <tsargent@gmail.com>
2015-10-06 13:55:34 -0400

    show the simple select component on forms page

--- src/js/pages/forms.js
+++ src/js/pages/forms.js
@@ -57,7 +57,10 @@ export default React.createClass({
             <TextField label="Text" fieldColor='light' placeholder="Placeholder" extraClasses={['py2']} />
             <NumberField label="Number" fieldColor='light' extraClasses={['py2']} units="Units"  />
             <DateField label="Date" fieldColor='light' extraClasses={['py2']} />
-            <SimpleSelect label="Simple Select" fieldColor='light' options={options} promptText="- Select -" extraClasses={['py2']}/>
+            <SimpleSelect
+              disabled={false}
+              options={options}
+              promptText="- Select -"/>
             <TextArea label="Textarea" fieldColor='light'  extraClasses={['py2']} />
             <TextArea label="Textarea Expandable" fieldColor='light' expandable={true} extraClasses={['py2']} />
             <Checkbox label="Checkbox" fieldColor='light' extraClasses={['py2']}/>
