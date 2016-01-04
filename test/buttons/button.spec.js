a4aaa48ad32f4679624a681548d4785c3af7ac3b
Katie Reed <katie.reed@namely.com>
2016-01-04 13:38:48 -0500

    Add 2nd test for rendering icons

--- test/buttons/button.spec.js
+++ test/buttons/button.spec.js
@@ -2,12 +2,19 @@ import { expect } from 'chai';
 import TestUtils from 'react/lib/ReactTestUtils';
 import React from 'react';
 import Button from 'buttons/button';
+import Sinon from 'sinon';
 
 describe('Button', () => {
 
   it('expects the button-link class if the link prop is true', () => {
     let button = TestUtils.renderIntoDocument(<Button link={true}/>)
     let options = TestUtils.scryRenderedDOMComponentsWithClass(button, 'button-link')
+    expect(options.length).to.equal(1)
+  });
+
+  it('renders an icon if icon exists', () => {
+    let button = TestUtils.renderIntoDocument(<Button icon={'pencil'}/>)
+    let options = TestUtils.scryRenderedDOMComponentsWithClass(button, 'icon-pencil')
     expect(options.length).to.equal(1)
   });
 
