978d9b04c6cc11aac25bf02919c2215a3b46a4a0
Jason Fellows <jasonfellows@gmail.com>
2016-01-12 14:25:10 -0500

    Update simple-select.spec

--- test/forms/fields/select/simple-select.spec.js
+++ test/forms/fields/select/simple-select.spec.js
@@ -39,7 +39,6 @@ describe('SimpleSelect', () => {
       simple_select.setState({show_options: true})
       TestUtils.Simulate.click(simple_select.refs.simpleSelectValue)
       expect(simple_select.state.show_options).to.be.false
-
     })
   });
 
@@ -60,21 +59,6 @@ describe('SimpleSelect', () => {
       it('returns bc-orange border color if there is an error', () => {
         let simple_select = TestUtils.renderIntoDocument(<SimpleSelect hasError={true}/>)
         let elem = TestUtils.scryRenderedDOMComponentsWithClass(simple_select, 'bc-orange bc-orange-hover');
-        expect(elem).to.exist
-      })
-      it('returns grey-25 border color if fieldColor is light', () => {
-        let simple_select = TestUtils.renderIntoDocument(<SimpleSelect fieldColor={('light')}/>)
-        let elem = TestUtils.scryRenderedDOMComponentsWithClass(simple_select, 'bc-grey-25');
-        expect(elem).to.exist
-      })
-      it('returns bc-white border color if the fieldColor is dark', () => {
-        let simple_select = TestUtils.renderIntoDocument(<SimpleSelect fieldColor={('dark')}/>)
-        let elem = TestUtils.scryRenderedDOMComponentsWithClass(simple_select, 'bc-white');
-        expect(elem).to.exist
-      })
-      it('returns grey-10 border color if the field is disabled', () => {
-        let simple_select = TestUtils.renderIntoDocument(<SimpleSelect fieldColor={('dark')} disabled={true}/>)
-        let elem = TestUtils.scryRenderedDOMComponentsWithClass(simple_select, 'bc-grey-10');
         expect(elem).to.exist
       })
     });
