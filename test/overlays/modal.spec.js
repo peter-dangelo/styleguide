e833d599fa8c140ed84c9451a25e3ec0fe1d8567
Katie Reed <katie.reed@namely.com>
2016-01-15 13:37:42 -0500

    Add sinon for callbacks, more tests

--- test/overlays/modal.spec.js
+++ test/overlays/modal.spec.js
@@ -2,11 +2,16 @@ import { expect } from 'chai';
 import TestUtils from 'react/lib/ReactTestUtils';
 import React from 'react';
 import Modal from 'overlays/modal';
+import Sinon from 'sinon';
 
 describe('Modal', () => {
+  let callback = Sinon.spy();
+
   let props = {
+    children: 'Child content',
+    closeModal: callback,
     isOpen: false,
-    children: 'Child content'
+    contentColumns: 5
   }
 
   let component;
@@ -18,7 +23,7 @@ describe('Modal', () => {
     modal = React.findDOMNode(component);
   })
 
-  it('should render successfully', () => {
+  it('renders successfully', () => {
     TestUtils.isElement(component);
   });
 
@@ -28,11 +33,34 @@ describe('Modal', () => {
 
   it('renders child content passed in through props', () => {
     expect(modal.innerHTML).to.contain(props.children);
-  })
+  });
+
+  it('adds the correct background classes', () => {
+    expect(modal.innerHTML).to.contain(props.children);
+  });
+
+  it('fires the onClick prop when clicked', () => {
+    TestUtils.Simulate.click(modal);
+    expect(callback.called).to.be.true;
+  });
 
   describe('Open Modal', () => {
 
-  
+    let componentOpen;
+    beforeEach(() => {
+      componentOpen = TestUtils.renderIntoDocument(
+        <Modal {...props} isOpen={true} />
+      )
+    });
+
+    it('sets the isOpen state to true if the isOpen prop is true', () => {
+      expect(componentOpen.state.isOpen).to.be.true;
+    });
+
+    it('adds the correct column class', () => {
+      let element = TestUtils.findRenderedDOMComponentWithClass(componentOpen, 'col-5');
+      expect(element).to.exist;
+    });
   });
 
 });
